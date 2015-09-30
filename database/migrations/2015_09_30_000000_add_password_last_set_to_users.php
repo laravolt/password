868cfbcf80de454332d1be23fa944dce67cb9e51
uyab <uyab.exe@gmail.com>
2015-09-30 11:35:35 +0700

    add migration

---
+++ database/migrations/2015_09_30_000000_add_password_last_set_to_users.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddPasswordLastSetToUser extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->timestamp('password_last_set')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('password_last_set');
+        });
+    }
+}
