5236aeff06c9f01ba7f49e0d91a687adf90fee03
Bayu Hendra Winata <bayu.hendra@javan.co.id>
2016-01-07 19:21:53 +0700

    update to Laravel 5.2

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -8,15 +8,12 @@ use Illuminate\Support\ServiceProvider as BaseServiceProvider;
  * Class PackageServiceProvider
  *
  * @package Laravolt\Password
- * @see http://laravel.com/docs/5.1/packages#service-providers
- * @see http://laravel.com/docs/5.1/providers
  */
 class ServiceProvider extends BaseServiceProvider
 {
     /**
      * Indicates if loading of the provider is deferred.
      *
-     * @see http://laravel.com/docs/5.1/providers#deferred-providers
      * @var bool
      */
     protected $defer = false;
@@ -24,39 +21,32 @@ class ServiceProvider extends BaseServiceProvider
     /**
      * Register the service provider.
      *
-     * @see http://laravel.com/docs/5.1/providers#the-register-method
      * @return void
      */
     public function register()
     {
         $this->app->singleton('password', function($app){
             $app['config']['auth.password.email'] = $app['config']['password.emails.reset'];
-            return new Password($app['auth.password'], $app['mailer'], $app['config']['password.emails.new']);
+            return new Password($app['auth.password.broker'], $app['mailer'], $app['config']['password.emails.new']);
         });
     }
 
     /**
      * Application is booting
      *
-     * @see http://laravel.com/docs/5.1/providers#the-boot-method
      * @return void
      */
     public function boot()
     {
-
         $this->registerViews();
         $this->registerMigrations();
-        $this->registerSeeds();
-        $this->registerAssets();
         $this->registerTranslations();
         $this->registerConfigurations();
-
     }
 
     /**
      * Register the package views
      *
-     * @see http://laravel.com/docs/5.1/packages#views
      * @return void
      */
     protected function registerViews()
@@ -72,7 +62,6 @@ class ServiceProvider extends BaseServiceProvider
     /**
      * Register the package migrations
      *
-     * @see http://laravel.com/docs/5.1/packages#publishing-file-groups
      * @return void
      */
     protected function registerMigrations()
@@ -83,34 +72,8 @@ class ServiceProvider extends BaseServiceProvider
     }
 
     /**
-     * Register the package database seeds
-     *
-     * @return void
-     */
-    protected function registerSeeds()
-    {
-        $this->publishes([
-            $this->packagePath('database/seeds') => database_path('/seeds')
-        ], 'seeds');
-    }
-
-    /**
-     * Register the package public assets
-     *
-     * @see http://laravel.com/docs/5.1/packages#public-assets
-     * @return void
-     */
-    protected function registerAssets()
-    {
-        $this->publishes([
-            $this->packagePath('resources/assets') => public_path('laravolt/password'),
-        ], 'public');
-    }
-
-    /**
      * Register the package translations
      *
-     * @see http://laravel.com/docs/5.1/packages#translations
      * @return void
      */
     protected function registerTranslations()
@@ -121,7 +84,6 @@ class ServiceProvider extends BaseServiceProvider
     /**
      * Register the package configurations
      *
-     * @see http://laravel.com/docs/5.1/packages#configuration
      * @return void
      */
     protected function registerConfigurations()
